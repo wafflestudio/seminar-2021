48121ad72f7ec9580d57ddc7ea11aa7d5ce6f691
woohm402 <woohm404@gmail.com>
2021-08-23 22:01:41 +0900

    html-css-js 예시 추가

---
+++ react/seminar0/html-css-js-example/example.js
@@ -0,0 +1,13 @@
+const sayReact = () => {
+    // arrow function
+    console.log('react');
+}
+
+function alertMe() {
+    console.log('그 윈도우 경고창 띄우는 그거')
+    window.alert('test alert 입니다')
+}
+
+console.log('js 파일은 지금 연결돼요')
+
+console.debug('디버그 모드도 있음')
