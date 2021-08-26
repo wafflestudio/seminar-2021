33b21085390ca4e71ba45ec14b4ebd7056f2f89c
woohm402 <woohm404@gmail.com>
2021-08-26 20:20:04 +0900

    example 내용 추가

--- react/seminar0/html-css-js-example/example.js
+++ react/seminar0/html-css-js-example/example.js
@@ -8,6 +8,25 @@ function alertMe() {
     window.alert('test alert 입니다')
 }
 
+const addRToExampleBox = () => {
+    const exampleBox = document.getElementById('example-box');
+    const text = exampleBox.innerText;
+    exampleBox.innerText = text + "R";
+}
+
+const alertInputValue = () => {
+    const input = document.getElementById('example-input');
+    const text = input.value;
+    window.alert(text);
+}
+
+const addElem = () => {
+    const ul = document.getElementById('example-ul');
+    const newLi = document.createElement('li');
+    newLi.innerText = `${Math.random()}`;
+    ul.appendChild(newLi);
+}
+
 console.log('js 파일은 지금 연결돼요')
 
 console.debug('디버그 모드도 있음')
