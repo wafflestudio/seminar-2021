0477be04a832321aa7010099de7390feeb1816d8
woohm402 <woohm404@gmail.com>
2021-09-01 20:28:44 +0900

    React 예제 작성

---
+++ react/seminar1/counter-example/5.counter-example-final/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Counter from "./Counter";
+
+const App = () => {
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+
+  return (
+    <div>
+      <Counter value={value1} incrementValue={() => setValue1(value1 + 1)} />
+      <br />
+      <Counter value={value2} incrementValue={() => setValue2(value2 + 1)} />
+      <br /><br /><br />
+      <p>
+        value1 + value2 = {value1 + value2}
+      </p>
+    </div>
+  );
+}
+
+export default App;
+
+
