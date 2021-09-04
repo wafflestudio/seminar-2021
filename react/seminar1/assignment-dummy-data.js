cded500c34093d81736af8dad31a92a723701e6a
woohm402 <39977696+woohm402@users.noreply.github.com>
2021-09-04 18:18:57 +0900

    Update assignment-dummy-data.js

--- react/seminar1/assignment-dummy-data.js
+++ react/seminar1/assignment-dummy-data.js
@@ -1,35 +1,42 @@
 const dummyData = [
     {
+        id: 1,
         name: '깃허브',
         grade: 1,
         profileImg: 'https://github.githubassets.com/images/modules/logos_page/Octocat.png',
     },
     {
+        id: 2,
         name: '빈학생',
         grade: 1,
         profileImg: '',
     },
     {
+        id: 3,
         name: '리액트',
         grade: 2,
         profileImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2560px-React-icon.svg.png',
     },
     {
+        id: 4,
         name: '스벨트',
         grade: 2,
         profileImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/1200px-Svelte_Logo.svg.png',
     },
     {
+        id: 5,
         name: '리덕스',
         grade: 3,
         profileImg: 'https://raw.githubusercontent.com/1ambda/1ambda.github.io/master/assets/images/redux/redux_logo.png?width=30%&height=30%',
     },
     {
+        id: 6,
         name: '타스',
         grade: 3,
         profileImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png',
     },
     {
+        id: 7,
         name: '싸쓰',
         grade: 3,
         profileImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/1280px-Sass_Logo_Color.svg.png',
