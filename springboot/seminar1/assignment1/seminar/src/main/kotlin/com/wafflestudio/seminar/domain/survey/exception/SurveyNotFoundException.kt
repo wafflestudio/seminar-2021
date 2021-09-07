44fafcb3936eae7171ca70b9931ecbf1483b9059
Hankyeol Choi <zlzlqlzl1@wafflestudio.com>
2021-09-07 22:10:30 +0900

    Update SurveyNotFoundException.kt

--- springboot/seminar1/assignment1/seminar/src/main/kotlin/com/wafflestudio/seminar/domain/survey/exception/SurveyNotFoundException.kt
+++ springboot/seminar1/assignment1/seminar/src/main/kotlin/com/wafflestudio/seminar/domain/survey/exception/SurveyNotFoundException.kt
@@ -2,4 +2,4 @@ package com.wafflestudio.seminar.domain.survey.exception
 
 import com.wafflestudio.seminar.common.exception.WaffleException
 
-class SurveyNotFoundException : WaffleException("OS NOT FOUND")
+class SurveyNotFoundException : WaffleException("SURVEY NOT FOUND")
